f4c03d1a3166494274ad8ac473af56729f725c30
ryandesnick <71520946+ryandesnick@users.noreply.github.com>
2021-05-17 09:52:58 -0400

    js1

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,17 @@ import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
 const key = "298075655132777";
+
+const query = async function () {
+  try {
+    const response = await fetch(
+      `https://www.superheroapi.com/api.php/298075655132777/80`
+    );
+    const data = await response.json();
+    console.log("data blahblahblah");
+  } catch (error) {
+    console.log(error);
+    alert("bro your code aint working");
+  }
+};
+query();
