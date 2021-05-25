4935005151e98eda1ecf1b8fa3606dee6392000a
nycallan <chernomorets3@gmail.com>
2021-05-24 20:19:52 -0400

    update

--- src/js/index.js
+++ src/js/index.js
@@ -9,16 +9,14 @@ const query = async function () {
       "https://superheroapi.com/api/4026085827461072/search/batman"
     );
     const data = await response.json();
-    console.log(data)
+    console.log(data);
     data.results.forEach((superhero) => {
       //DOMSelectors.grid.insertAdjacentHTML("beforeend", "");
       console.log(superhero);
     });
-    
   } catch (error) {
     console.log(error);
     alert("Something is Wrong");
   }
 };
 query();
-
