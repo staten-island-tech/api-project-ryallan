f7279e3a1230599e4727ea6d3f1eb9c3d8fc3a8b
Ryan Desnick <ryandesnick@gmail.com>
2021-06-02 00:00:01 -0400

    search 1

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,5 +1,7 @@
 const DOMSelectors = {
   grid: document.querySelector(".superhero-grid"),
+  searchForm: document.getElementById("search-form"),
+  searchArea: document.getElementById("search-area"),
 };
 
 export { DOMSelectors };
---
+++ src/js/search.js
@@ -0,0 +1,38 @@
+import { DOMSelectors } from "./DOM";
+
+const listen = function() {
+    DOMSelectors.searchForm.addEventListener("submit", function(e) {
+        e.preventDefault();
+        const searchParams = DOMSelectors.searchArea.value;
+        const searchQuery = async function () {
+            try {
+              const response = await fetch(
+                "https://superheroapi.com/api/4026085827461072/search/spider-man"
+              );
+              const data = await response.json();
+              console.log(data)
+              data.results.forEach((superhero) => {
+                DOMSelectors.grid.insertAdjacentHTML("beforeend", `<div class="superhero-card">
+                <div class="superhero-front">
+                 <img
+                   src= "${superhero.image.url}"
+                   alt=""
+                   class="poster"
+                 />
+               </div>
+               <div class="superhero-card-back">
+                    <h3 class="superhero-card-header">${superhero.name}</h3>
+                    <div class="full-name">
+                      <p class="whole-name">Alignment</p>
+                      <p class="whole-name">${superhero.biography.alignment}</p>
+                    </div>`);
+                console.log(superhero);
+              });
+              
+            } catch (error) {
+              console.log(error);
+              alert("Something is Wrong");
+            }
+          };
+    });
+}
